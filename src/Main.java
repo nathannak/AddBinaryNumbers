aca3e7513e1526b65e9ff04846c739868f018cbd
nn <a%liKh2&>
2019-06-01 18:48:00 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(addBinary("1010","1011"));
+
+    }
+
+    public static String addBinary(String a, String b) {
+        int lena = a.length();
+        int lenb = b.length();
+        int i =0, carry = 0;
+        String res = "";
+        while(i<lena || i<lenb || carry!=0){
+            int x = (i<lena) ? Character.getNumericValue(a.charAt(lena - 1 - i)) : 0;
+            int y = (i<lenb) ? Character.getNumericValue(b.charAt(lenb - 1 - i)) : 0;
+            res = (x + y + carry)%2 + res;
+            carry = (x + y + carry)/2;
+            i++;
+        }
+        return res;
+    }
+
+}
+
